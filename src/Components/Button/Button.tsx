386b9be2e2e37cab010d4b5004982cedcabe0968
Mohammad Ahmad <mohammad.ahmad@carbonteq.com>
2022-08-18 17:12:28 +0500

    fixed glitches

--- src/Components/Button/Button.tsx
+++ src/Components/Button/Button.tsx
@@ -1,32 +1,28 @@
-import React from "react"
-import ReactDOm from "react-dom"
-
-
-interface Props
-{
-    DisplayText: string;
-    key?:number;
-    onClick: (e:any) => void;
-    buttonClass: string;
-    buttonTextClass: string;
-
-
+import React from "react";
+import ReactDOm from "react-dom";
 
 interface Props {
   DisplayText: string;
   key?: number;
-  onClick: any;
+  onClick: (e: any) => void;
+  buttonClass: string;
+  buttonTextClass: string;
 }
 
-const Button: React.FC<Props> = ({DisplayText, key, onClick, buttonClass, buttonTextClass}) =>{
-
-    return (
-        <>
-        <div className={buttonClass} key={key} onClick={onClick}>
-            <h1 className={buttonTextClass}>{DisplayText}</h1>
-        </div>
-        </>
-    )
-
+const Button: React.FC<Props> = ({
+  DisplayText,
+  key,
+  onClick,
+  buttonClass,
+  buttonTextClass,
+}) => {
+  return (
+    <>
+      <div className={buttonClass} key={key} onClick={onClick}>
+        <h1 className={buttonTextClass}>{DisplayText}</h1>
+      </div>
+    </>
+  );
+};
 
 export default Button;
